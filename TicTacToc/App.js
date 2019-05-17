5f4097c713ebf191b6a95d8c9ff800c2ad9397bd
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-05-17 11:08:12 -0400

    dando estilo a mi Grilla 3x3

--- TicTacToc/App.js
+++ TicTacToc/App.js
@@ -7,21 +7,29 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={{ flexDirection: 'row' }}>
-          <View style={styles.tile} />
-          <View style={styles.tile} />
-          <View style={styles.tile} />
+          <View
+            style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]}
+          />
+          <View style={[styles.tile, { borderTopWidth: 0 }]} />
+          <View
+            style={[styles.tile, { borderTopWidth: 0, borderRightWidth: 0 }]}
+          />
         </View>
 
         <View style={{ flexDirection: 'row' }}>
-          <View style={styles.tile} />
-          <View style={styles.tile} />
-          <View style={styles.tile} />
+          <View style={[styles.tile, { borderLeftWidth: 0 }]} />
+          <View style={[styles.tile, {}]} />
+          <View style={[styles.tile, { borderRightWidth: 0 }]} />
         </View>
 
         <View style={{ flexDirection: 'row' }}>
-          <View style={styles.tile} />
-          <View style={styles.tile} />
-          <View style={styles.tile} />
+          <View
+            style={[styles.tile, { borderBottomWidth: 0, borderLeftWidth: 0 }]}
+          />
+          <View style={[styles.tile, { borderBottomWidth: 0 }]} />
+          <View
+            style={[styles.tile, { borderBottomWidth: 0, borderRightWidth: 0 }]}
+          />
         </View>
       </View>
     );
