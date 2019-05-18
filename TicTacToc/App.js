9d15ef98a054aed39ec8934f5fc9169080583c9c
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-05-18 19:31:30 -0400

    muestra jugador Ganador

--- TicTacToc/App.js
+++ TicTacToc/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  Button
+} from 'react-native';
 import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons';
 
 export default class App extends React.Component {
   // punto de partida de la App
+
   constructor(props) {
     super(props);
+
     this.state = {
       gameState: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
       currentPlayer: 1 // first player
@@ -17,31 +26,34 @@ export default class App extends React.Component {
   }
 
   initializeGame = () => {
-    this.setState({ gameState: [[0, 0, 0], [0, 0, 0], [0, 0, 0]] });
+    this.setState({
+      gameState: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+      currentPlayer: 1
+    });
   };
 
   // Capturar al Ganador
   getWinner = () => {
     const NUM_TILES = 3;
-    let matriz = this.state.gameState;
-    let suma;
+    var matriz = this.state.gameState;
+    var suma;
 
     // comprobando Fila ganadora
-    for (let i = 0; i < NUM_TILES; i++) {
+    for (var i = 0; i < NUM_TILES; i++) {
       suma = matriz[i][0] + matriz[i][1] + matriz[i][2];
       if (suma == 3) {
         return 1;
-      } else if ((suma = -3)) {
+      } else if (suma == -3) {
         return -1;
       }
     }
 
     // comprobando columna ganadora
-    for (let i = 0; i < NUM_TILES; i++) {
+    for (var i = 0; i < NUM_TILES; i++) {
       suma = matriz[0][i] + matriz[1][i] + matriz[2][i];
       if (suma == 3) {
         return 1;
-      } else if ((suma = -3)) {
+      } else if (suma == -3) {
         return -1;
       }
     }
@@ -50,36 +62,53 @@ export default class App extends React.Component {
     suma = matriz[0][0] + matriz[1][1] + matriz[2][2];
     if (suma == 3) {
       return 1;
-    } else if ((suma = -3)) {
+    } else if (suma == -3) {
       return -1;
     }
 
     suma = matriz[2][0] + matriz[1][1] + matriz[0][2];
     if (suma == 3) {
       return 1;
-    } else if ((suma = -3)) {
+    } else if (suma == -3) {
       return -1;
     }
+
+    // comprobando cuando No existe ganador
+    return 0;
   };
 
   onTilePress = (row, col) => {
     // No permite reescribir una ficha ya marcada
-    let value = this.state.gameState[row][col];
+    var value = this.state.gameState[row][col];
     if (value !== 0) {
       return;
     }
 
     // captura jugador actual
-    let currentPlayer = this.state.currentPlayer;
+    var currentPlayer = this.state.currentPlayer;
 
     // establecer posición de la ficha
-    let matriz = this.state.gameState.slice(); // devuelve una copia nueva
+    var matriz = this.state.gameState.slice(); // devuelve una copia nueva
     matriz[row][col] = currentPlayer;
     this.setState({ gameState: matriz });
 
     // Siguiente jugador
-    let nextPlayer = currentPlayer == 1 ? -1 : 1;
+    var nextPlayer = currentPlayer == 1 ? -1 : 1;
     this.setState({ currentPlayer: nextPlayer });
+
+    // Comprobar al Ganador
+    var winner = this.getWinner();
+    if (winner == 1) {
+      Alert.alert('Jugador 1 es el Ganador');
+      this.initializeGame();
+    } else if (winner == -1) {
+      Alert.alert('Jugador 2 es el Ganador');
+      this.initializeGame();
+    }
+  };
+
+  onNewGamePress = () => {
+    this.initializeGame();
   };
 
   renderIcon = (row, col) => {
@@ -165,6 +194,8 @@ export default class App extends React.Component {
             {this.renderIcon(2, 2)}
           </TouchableOpacity>
         </View>
+        <View style={{ paddingTop: 50 }} />
+        <Button title="Nuevo Juego" onPress={this.onNewGamePress} />
       </View>
     );
   }
@@ -181,22 +212,18 @@ const styles = StyleSheet.create({
   tile: {
     borderWidth: 8,
     width: 100,
-    height: 100
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 
   tileX: {
     color: 'red',
-    fontSize: 60,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    fontSize: 60
   },
 
   tileO: {
     color: 'green',
-    fontSize: 60,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    fontSize: 60
   }
 });
